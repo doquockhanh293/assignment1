08c7efd7335e4ea172569eba05b379afbf8b37c2
doquockhanh <dqkhanh293@gmail.com>
2024-09-30 07:11:16 +0700

    assignment_fix_app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const quizRoutes = require('./src/routes/quizRoutes');
 const questionRoutes = require('./src/routes/questionRoutes');
-
+require('dotenv').config();
 const app = express();
 
 // Middleware
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://doquockhanh:doquockhanh@doquockhanh.idqr4.mongodb.net/SimpleQuiz', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`${process.env.MONGODB_URL}`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
